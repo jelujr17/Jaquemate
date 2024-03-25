dd40685eff5ea92c146ca07a9368b97e26938655
Mario <marioml7902@gmail.com>
2024-03-25 14:15:25 +0100

    sasas

--- vista/layout/cabecera.php
+++ vista/layout/cabecera.php
@@ -27,6 +27,8 @@
   </title>
   <meta charset="utf-8">
   <link rel="stylesheet" href="css/cabecera.css">
+  <link rel="stylesheet" href="css/realidadvirtual.css">
+
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="icon" href="assets/icono.png" type="image/png"> <!-- Especifica la ruta de tu ícono y su tipo -->
