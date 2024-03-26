e95a784a4fbea18961d7be9a4192476f222bbff3
PabloPintoBiosca <pablopintob.1@email.com>
2024-03-26 22:31:02 +0100

    a ver que tal

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
     <p class="col">JaqueMate</p>
     <h3 class="col">Mayoristas S.L</h3>
 </div>
-<div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-5 py-5 top-50 start-50 translate-middle-x h-50 divSecundario">
-      <div class="col appear-from-bottom">
-        <div class="card card-cover h-75 overflow-hidden text-bg-dark rounded-4 shadow-lg opacidad">
-          <div class="d-flex flex-column h-100 p-5 pb-3 text-white">
+<div class="row row-cols-sg-1 row-cols-lg-3 align-items-stretch g-5 py-5 top-50 start-50 translate-middle-x divSecundario pt-0">
+      <div class="col appear-from-bottom pt-0">
+        <div class="card card-cover overflow-hidden text-bg-dark rounded-4 shadow-lg opacidad ">
+          <div class="d-flex flex-column p-5 pb-3 text-white">
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-graph-up-arrow" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M0 0h1v15h15v1H0zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5"/>
           </svg>
-            <h3 class="pt-1 display-6 fw-bold">Potencia tu marca</h3>
+            <h3 class="pt-lg-1 display-lg-6 display-mg-6 fw-bold">Potencia tu marca</h3>
             <ul class="d-flex list-unstyled mt-auto">
               <li class="me-auto">holac aasdasd adsd </li>
             </ul>
@@ -24,13 +24,13 @@
       </div>
 
       <div class="col appear-from-bottom">
-        <div class="card card-cover h-75 overflow-hidden text-bg-dark rounded-4 shadow-lg opacidad">
+        <div class="card card-cover overflow-hidden text-bg-dark rounded-4 shadow-lg opacidad">
           <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-headset-vr" viewBox="0 0 16 16">
             <path d="M8 1.248c1.857 0 3.526.641 4.65 1.794a5 5 0 0 1 2.518 1.09C13.907 1.482 11.295 0 8 0 4.75 0 2.12 1.48.844 4.122a5 5 0 0 1 2.289-1.047C4.236 1.872 5.974 1.248 8 1.248"/>
             <path d="M12 12a4 4 0 0 1-2.786-1.13l-.002-.002a1.6 1.6 0 0 0-.276-.167A2.2 2.2 0 0 0 8 10.5c-.414 0-.729.103-.935.201a1.6 1.6 0 0 0-.277.167l-.002.002A4 4 0 1 1 4 4h8a4 4 0 0 1 0 8"/>
           </svg>
-          <h3 class="pt-1 display-6 fw-bold">Realidad virtual</h3>
+          <h3 class="pt-1 display-lg-6 display-mg-6 fw-bold">Realidad virtual</h3>
             <ul class="d-flex list-unstyled mt-auto">
               <li class="me-auto">holac aasdasd adsd </li>
             </ul>
@@ -39,13 +39,13 @@
       </div>
 
       <div class="col appear-from-bottom">
-        <div class="card card-cover h-75 overflow-hidden text-bg-dark rounded-4 shadow-lg opacidad">
+        <div class="card card-cover overflow-hidden text-bg-dark rounded-4 shadow-lg opacidad">
           <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-code-square" viewBox="0 0 16 16">
             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
             <path d="M6.854 4.646a.5.5 0 0 1 0 .708L4.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0m2.292 0a.5.5 0 0 0 0 .708L11.793 8l-2.647 2.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708 0"/>
           </svg>
-          <h3 class="pt-1 display-6 fw-bold">Soluciones digitales</h3>
+          <h3 class="pt-1 display-lg-6 display-mg-6 fw-bold solu">Soluciones digitales</h3>
           <ul class="d-flex list-unstyled mt-auto">
               <li class="me-auto">holac aasdasd adsd </li>
             </ul>
@@ -56,13 +56,13 @@
 </div>
 
 
-<div class="container-fluid mt-5 pt-5 fondo pb-5">
+<div class="container-fluid mt-5 pt-5 fondo">
 <div class="row row-cols-1 row-cols-lg-3 g-3">
   <div class="col">
     <div class="card shadow-sm contenido">
     <div class="d-flex align-items-center position-relative">
-      <div class="diagonal-bg position-absolute top-0 bottom-0 start-0 end-50"></div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-right-dots" viewBox="0 0 16 16" >
+      <div class="diagonal-bg position-absolute top-0  start-0 end-50"></div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" fill="currentColor" class="bi bi-chat-right-dots" viewBox="0 0 16 16" >
           <path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/>
           <path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
         </svg>
@@ -73,7 +73,7 @@
   <div class="col">
     <div class="card shadow-sm contenido">
       <div class="d-flex align-items-center">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+      <svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
         <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
       </svg>
@@ -84,7 +84,7 @@
   <div class="col">
     <div class="card shadow-sm contenido">
       <div class="d-flex align-items-center">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-medical" viewBox="0 0 16 16">
+        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" fill="currentColor" class="bi bi-file-earmark-medical" viewBox="0 0 16 16">
           <path d="M7.5 5.5a.5.5 0 0 0-1 0v.634l-.549-.317a.5.5 0 1 0-.5.866L6 7l-.549.317a.5.5 0 1 0 .5.866l.549-.317V8.5a.5.5 0 1 0 1 0v-.634l.549.317a.5.5 0 1 0 .5-.866L8 7l.549-.317a.5.5 0 1 0-.5-.866l-.549.317zm-2 4.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1z"/>
           <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
         </svg>
