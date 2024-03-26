94bed0d6d11086f45a5be2c0f88b3eb8f7573863
Mario <marioml7902@gmail.com>
2024-03-26 08:09:49 +0100

    realidad virtual

--- vista/realidadvirtual.php
+++ vista/realidadvirtual.php
@@ -1,13 +1,12 @@
 <?php include 'layout/cabecera.php'; ?>
-<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-    <main class="px-3">
-        <h1>Cover your page.</h1>
-        <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the
+<div class="cont">
+    <main class="tituloVidaReal">
+        <h1>Vida Real.</h1>
+        <p class="textoVidaReal">Cover is a one-page template for building simple and beautiful home pages. Download, edit the
             text, and add your own fullscreen background photo to make it your own.</p>
         <p class="botonVerMas">
-            <a href="#" class="verMas">Learn more</a>
+            <a href="#" class="verMas">Ver más</a>
         </p>
     </main>
 </div>
-
 <?php include 'layout/footer.php'; ?>
