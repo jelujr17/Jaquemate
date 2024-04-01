d703ca1c2c0fea9af6e6840b28783b00b5a77636
Pablo Pinto <pablopintob.1@gmail.com>
2024-04-02 00:15:28 +0200

    g

--- potenciar.php
+++ potenciar.php
@@ -1,8 +1,32 @@
 <?php include 'vista/layout/cabecera.php'; ?> 
+<link rel="stylesheet" href="vista/css/potenciar.css">
 
-<div>
-
+<div id="marcas" class="h-25">
+    <p>marcas</p>
+</div>  
+<div id="texto" class="mt-3 mb-4">
+    <div id="columnas">
+    <p class="p1">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que también ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenían pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+    <p class="p2">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que también ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenían pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+    </div>
+    <p class="p3">GRATIS</p>
+</div>
+<hr class="drop">
+<div class="mitad mt-4">
+    <img src="vista/assets/abuelos_portada.png" alt="">
+    <p class="m-1">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+</div>
+<div class="mitad mt-2">
+    <p class="m-2">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+    <img src="vista/assets/abuelos_portada.png" alt="">
 </div>
 
+
+<script>
+    var div = document.querySelector('.containerCabecera');
+    var carousel = document.querySelector('#marcas');
+    var altura = div.clientHeight;
+    carousel.style.marginTop = altura + 'px';
+</script>
 
 <?php include 'vista/layout/footer.php';?>
--- realidadvirtual.php
+++ realidadvirtual.php
@@ -1,4 +1,5 @@
 <?php include 'vista/layout/cabecera.php'; ?>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&display=swap">
 <link rel="stylesheet" href="vista/css/realidadvirtual.css">
 
 <div id="imgContent">
@@ -7,6 +8,12 @@
         </div>
         <p id="izq">SOLUCIONES</p><p id="izq2">REALES</p>
         <p id="der">EN REALIDAD</p><p id="der2">VIRTUAL</p>
+</div>
+<div id="conocer" class="container-fluid mt-5">
+    <div id="cuadro" class="w-75 p-4 rounded shadow">
+        <p class="lead">¿Quieres conocernos mejor?</p>
+        <a href="#" class="btn btn-primary btn-lg mt-3">Visitar nuestra web</a>
+    </div>
 </div>
 
 <script src="vista/js/realidadvirtual.js"></script>
