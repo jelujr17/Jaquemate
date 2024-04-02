605e4b728a285fd5a4693939ce79eb9dfffba91c
Mario <marioml7902@gmail.com>
2024-04-02 09:49:32 +0200

    jodete pablo

--- index.php
+++ index.php
@@ -1,21 +1,13 @@
-<?php require_once('vista/layout/cabecera.php'); ?>
-
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link rel="stylesheet" href="vista/css/index.css">
-<div class="container-fluid px-4 py-5 fondoimagen position-relative h-100" id="custom-cards">
-<div class="text-container row-cols-1 start-75 align-items-stretch g-5 py-5 start-50 translate-middle-x appear-from-right2">
-    <p class="col">JaqueMate</p>
-    <h3 class="col">Mayoristas S.L</h3>
-</div>
-<div class="row row-cols-sg-1 row-cols-lg-3 align-items-stretch g-5 py-5 top-50 start-50 translate-middle-x divSecundario h-100">
-      <div class="col appear-from-bottom pt-0">
-        <div class="card card-cover overflow-hidden text-bg-dark rounded-4 shadow-lg opacidad ">
-          <div class="d-flex flex-column p-5 pb-3 text-white">
-          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-graph-up-arrow" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M0 0h1v15h15v1H0zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5"/>
-          </svg>
-            <h3 class="pt-lg-1 display-6 display-mg-4 fw-bold">Potencia tu marca</h3>
+<div class="container px-4 py-5 h-100 fondoimagen" id="custom-cards">
+
+    <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-5 py-5 pt-5 mt-5">
+      <div class="col">
+        <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
+          <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+            <h3 class="pt-5 display-7 fw-bold">Short title, long jacket</h3>
             <ul class="d-flex list-unstyled mt-auto">
               <li class="me-auto">holac aasdasd adsd </li>
             </ul>
@@ -23,76 +15,57 @@
         </div>
       </div>
 
-      <div class="col appear-from-bottom">
-        <div class="card card-cover overflow-hidden text-bg-dark rounded-4 shadow-lg opacidad">
-          <div class="d-flex flex-column p-5 pb-3 text-white text-shadow-1">
-          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-headset-vr" viewBox="0 0 16 16">
-            <path d="M8 1.248c1.857 0 3.526.641 4.65 1.794a5 5 0 0 1 2.518 1.09C13.907 1.482 11.295 0 8 0 4.75 0 2.12 1.48.844 4.122a5 5 0 0 1 2.289-1.047C4.236 1.872 5.974 1.248 8 1.248"/>
-            <path d="M12 12a4 4 0 0 1-2.786-1.13l-.002-.002a1.6 1.6 0 0 0-.276-.167A2.2 2.2 0 0 0 8 10.5c-.414 0-.729.103-.935.201a1.6 1.6 0 0 0-.277.167l-.002.002A4 4 0 1 1 4 4h8a4 4 0 0 1 0 8"/>
-          </svg>
-          <h3 class="pt-1 display-6 display-mg-6 fw-bold">Realidad virtual</h3>
+      <div class="col">
+        <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
+          <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+            <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Short title, long jacket</h3>
             <ul class="d-flex list-unstyled mt-auto">
-              <li class="me-auto">holac aasdasd adsd </li>
+              <li class="me-auto">
+
+              </li>
             </ul>
           </div>
         </div>
       </div>
 
-      <div class="col appear-from-bottom">
-        <div class="card card-cover overflow-hidden text-bg-dark rounded-4 shadow-lg opacidad">
-          <div class="d-flex flex-column p-5 pb-3 text-shadow-1">
-          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-code-square" viewBox="0 0 16 16">
-            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
-            <path d="M6.854 4.646a.5.5 0 0 1 0 .708L4.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0m2.292 0a.5.5 0 0 0 0 .708L11.793 8l-2.647 2.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708 0"/>
-          </svg>
-          <h3 class="pt-1 display-6 display-mg-6 fw-bold solu">Soluciones digitales</h3>
-          <ul class="d-flex list-unstyled mt-auto">
-              <li class="me-auto">holac aasdasd adsd </li>
+      <div class="col">
+        <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
+          <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+            <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Short title, long jacket</h3>
+            <ul class="d-flex list-unstyled mt-auto">
+              <li class="me-auto">
+              </li>
             </ul>
           </div>
         </div>
       </div>
     </div>
 </div>
-
-
-<div class="container-fluid mt-5 pt-5 fondo">
+<div class="container-fluid mt-5 pt-5">
 <div class="row row-cols-1 row-cols-lg-3 g-3">
   <div class="col">
-    <div class="card shadow-sm contenido">
-    <div class="d-flex align-items-center position-relative">
-      <div class="diagonal-bg position-absolute top-0  start-0 end-50"></div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" fill="currentColor" class="bi bi-chat-right-dots" viewBox="0 0 16 16" >
-          <path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/>
-          <path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
-        </svg>
-        <p class="mb-0 ps-3">CONTACTO</p>
-      </div>
-    </div>
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
   </div>
   <div class="col">
-    <div class="card shadow-sm contenido">
-      <div class="d-flex align-items-center">
-      <svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
-        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-        <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
-      </svg>
-        <p class="mb-0">INFÓRMATE</p>
-      </div>
+    <div class="card shadow-sm">
+    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+    <div class="card-body">
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
     </div>
+</div>
   </div>
   <div class="col">
-    <div class="card shadow-sm contenido">
-      <div class="d-flex align-items-center">
-        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" fill="currentColor" class="bi bi-file-earmark-medical" viewBox="0 0 16 16">
-          <path d="M7.5 5.5a.5.5 0 0 0-1 0v.634l-.549-.317a.5.5 0 1 0-.5.866L6 7l-.549.317a.5.5 0 1 0 .5.866l.549-.317V8.5a.5.5 0 1 0 1 0v-.634l.549.317a.5.5 0 1 0 .5-.866L8 7l.549-.317a.5.5 0 1 0-.5-.866l-.549.317zm-2 4.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1z"/>
-          <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
-        </svg>
-        <p class="mb-0">TÉRMINOS</p>
-      </div>
-    </div>
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
   </div>
 </div>
-</div>
-<script src="vista/js/index.js"></script>
 <?php include 'vista/layout/footer.php';?>
--- vista/layout/cabecera.php
+++ vista/layout/cabecera.php
@@ -1,4 +1,3 @@
-<html>
 <head>
   <?php
   $current_page = basename($_SERVER['PHP_SELF']);
@@ -28,25 +27,24 @@
   </title>
   <meta charset="utf-8">
   <link rel="stylesheet" href="vista/css/cabecera.css">
-  <link rel="stylesheet" href="vista/css/cabecera.css">
   <link rel="stylesheet" href="vista/css/realidadvirtual.css">
   <link rel="stylesheet" href="vista/css/soluciones.css">
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="icon" href="vista/assets/icono.png" type="image/png"> <!-- Especifica la ruta de tu ícono y su tipo -->
     <link rel="icon" href="vista/assets/icono.png" type="image/png"> 
     <script src="../vista/js/modo.js" defer></script>
 
   </head>
 
 <body>
-  <div class="containerCabecera oculto">
+  <div class="containerCabecera">
     <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start ">
       <a href="index.php" class="d-inline-flex link-body-emphasis text-decoration-none m-3">
         <img src="vista/assets/icono.png" alt="Logo" width="65" height="50">
-        <img src="vista/assets/icono.png" alt="Logo" width="65" height="50">
       </a>
+
+
       <?php
       // Obtener el nombre de la página actual
       $current_page = basename($_SERVER['PHP_SELF']);
@@ -88,4 +86,3 @@
 
     </div>
   </div>
-
