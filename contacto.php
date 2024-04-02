f4119a1376ad6e511189e9bf6f300c890411598f
Pablo Pinto <pablopintob.1@gmail.com>
2024-04-03 00:03:31 +0200

    vamo

--- contacto.php
+++ contacto.php
@@ -1,19 +1,26 @@
 <?php include 'vista/layout/cabecera.php'; ?>
-
+    <link rel="stylesheet" href="vista/css/contacto.css">
     <div class="p-5 pt-0 uno">
-        <div class="container-fluid py-5">
-            <h2 class="display-5 fw-bold">Correo</h2>
-            <p class="col-md-8 fs-4 border">Estamos aquí para ayudarte en todo lo que necesites. Si tienes alguna pregunta,
+        <div class="container-fluid py-5 ds2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-check" viewBox="0 0 16 16">
+                <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2zm3.708 6.208L1 11.105V5.383zM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2z"/>
+                <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0m-1.993-1.679a.5.5 0 0 0-.686.172l-1.17 1.95-.547-.547a.5.5 0 0 0-.708.708l.774.773a.75.75 0 0 0 1.174-.144l1.335-2.226a.5.5 0 0 0-.172-.686"/>
+            </svg>
+            <p class="col-md-8 fs-4">Estamos aquí para ayudarte en todo lo que necesites. Si tienes alguna pregunta,
                 comentario o consulta, no dudes en ponerte en contacto con nosotros. Puedes enviarnos un correo
                 electrónico a informacion@jaquemate.es y estaremos encantados de responderte lo antes posible. </p>
             <a href="mailto:informacion@jaquemate.es" class="btn btn-warning btn-lg" role="button">Contactar</a>
         </div>
     </div>
 
-    <div class="row row-cols-sg-1 row-cols-lg-2 g-5 start-50 w-75 h-auto mx-auto">
-        <div class="col-ld-6">
+    <div class="row row-cols-sg-1 row-cols-lg-2 g-5 start-50 w-75 h-auto mx-auto ds">
+        <div class="col">
             <div class="h-100 p-4 text-bg-dark rounded-3">
-                <h2 class="text-center">Teléfono</h2>
+                <div class="d-flex justify-content-center align-items-center mb-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+                        <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+                    </svg>
+                </div>
                 <p>Estamos disponibles para atenderte y resolver tus consultas. Si necesitas asistencia inmediata o
                     prefieres hablar con nosotros directamente, no dudes en llamarnos. Puedes contactarnos por teléfono
                     al 966 231 926 y nuestro equipo estará encantado de ayudarte en lo que necesites.
@@ -23,7 +30,11 @@
         </div>
         <div class="col-ld-6">
             <div class="h-100 p-4 bg-body-tertiary border rounded-3">
-                <h2 class="text-center">Dirección</h2>
+                <div class="d-flex justify-content-center align-items-center mb-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-map mx-auto" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M15.817.113A.5.5 0 0 1 16 .5v14a.5.5 0 0 1-.402.49l-5 1a.5.5 0 0 1-.196 0L5.5 15.01l-4.902.98A.5.5 0 0 1 0 15.5v-14a.5.5 0 0 1 .402-.49l5-1a.5.5 0 0 1 .196 0L10.5.99l4.902-.98a.5.5 0 0 1 .415.103M10 1.91l-4-.8v12.98l4 .8zm1 12.98 4-.8V1.11l-4 .8zm-6-.8V1.11l-4 .8v12.98z"/>
+                    </svg>
+                </div>
                 <p>Estamos ubicados en Alicante. Si deseas visitarnos en persona o enviarnos correspondencia,
                     nuestra dirección es la siguiente: Calle Jose Aldeguer, 7, 03016. Estamos convenientemente situados y
                     listos para recibirte en cualquier momento. ¡Esperamos verte pronto en nuestras instalaciones!</p>
--- soluciones.php
+++ soluciones.php
@@ -1,68 +1,7 @@
 <?php include 'vista/layout/cabecera.php'; ?>
-
+<link rel="stylesheet" href="vista/css/soluciones.css">
 <div>
-    <div id="myCarousel" class="carousel slide mb-6" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true"
-                aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
-                </svg>
-                <div class="container">
-                    <div class="carousel-caption text-start">
-                        <h1>Software</h1>
-                        <p class="opacity-75">Some representative placeholder content for the first slide of the
-                            carousel.</p>
-                        <p><a class="btn btn-lg btn-warning" href="#">Ver más</a></p>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <!-- Reemplaza la imagen de fondo aquí -->
-                <img src="vista/assets/servicios.png" class="d-block w-100" alt="...">
-                <div class="container">
-                    <div class="carousel-caption">
-                        <h1>Servicios</h1>
-                        <p>Hacemos realidad tus ideas digitales.</p>
-                        <p><a class="btn btn-lg btn-warning" href="#">Ver más</a></p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="carousel-item">
-                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
-                </svg>
-                <div class="container">
-                    <div class="carousel-caption text-end">
-                        <h1>Soluciones empresa</h1>
-                        <p>Some representative placeholder content for the third slide of this carousel.</p>
-                        <p><a class="btn btn-lg btn-warning" href="#">Ver productos</a></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
-
-
-
-    <div class="container marketing">
-        <hr class="featurette-divider">
+    <div class="container marketing pt-3">
         <div class="row featurette">
             <div class="col-md-7">
                 <h2 class="featurette-heading fw-normal lh-1">Siempre pensando en el <span
@@ -82,139 +21,114 @@
         </div>
         <hr class="featurette-divider">
     </div>
-
-
-    <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
-        <button class="btn btn-bd-primary py-2 dropdown-toggle d-flex align-items-center" id="bd-theme" type="button"
-            aria-expanded="false" data-bs-toggle="dropdown" aria-label="Toggle theme (auto)">
-            <svg class="bi my-1 theme-icon-active" width="1em" height="1em">
-                <use href="#circle-half"></use>
-            </svg>
-            <span class="visually-hidden" id="bd-theme-text">Toggle theme</span>
-        </button>
-        <ul class="dropdown-menu dropdown-menu-end px-1" aria-labelledby="bd-theme">
-            <li>
-                <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light">
-                    <svg class="bi me-2 opacity-50 theme-icon" width="16" height="16" fill="currentColor">
-                        <use href="#sun-fill"></use>
-                    </svg>
-                    Light
-                </button>
-            </li>
-            <li>
-                <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark">
-                    <svg class="bi me-2 opacity-50 theme-icon" width="16" height="16" fill="currentColor">
-                        <use href="#moon-stars-fill"></use>
-                    </svg>
-                    Dark
-                </button>
-            </li>
-            <li>
-                <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto">
-                    <svg class="bi me-2 opacity-50 theme-icon" width="16" height="16" fill="currentColor">
-                        <use href="#circle-half"></use>
-                    </svg>
-                    Auto
-                </button>
-            </li>
-        </ul>
-
-
-    </div>
-
-    <div class="container py-5">
-        <h1 class="text-body-emphasis">Descubre nuestra gama de software</h1>
-        <p class="fs-5 col-md-8">Explora nuestra gama de software, que incluye una variedad de herramientas diseñadas
-            para diferentes necesidades. Desde gestión residencial hasta productividad, nuestros productos
-            ofrecen soluciones eficientes. Algunos proyectos están aún en desarrollo para ofrecerte aún más opciones
-            innovadoras.</p>
-
-        <hr class="col-3 col-md-2 mb-5">
-
-        <div class="row g-5">
-            <div class="col-md-6">
-                <h2 class="text-body-emphasis">Nuestros proyectos</h2>
-                <p>Explora nuestra variedad de proyectos diseñados para cubrir diversas necesidades y ofrecer
-                    soluciones innovadoras. </p>
-                <ul class="list-unstyled ps-0">
-                    <li>
-                        <a class="icon-link mb-1" href="soluciones/tiemporeal.php" rel="noopener" target="_blank">
-                            <svg class="bi" width="16" height="16">
-                                <use xlink:href="soluciones/tiemporeal.php" />
-                            </svg>
-                            TiempoReal
-                        </a>
-                    </li>
-                    <li>
-                        <a class="icon-link mb-1" href="https://github.com/twbs/examples/tree/main/parcel"
-                            rel="noopener" target="_blank">
-                            <svg class="bi" width="16" height="16">
-                                <use xlink:href="#arrow-right-circle" />
-                            </svg>
-                            Control de almacén
-                        </a>
-                    </li>
-                    <li>
-                        <a class="icon-link mb-1" href="https://github.com/twbs/examples/tree/main/vite" rel="noopener"
-                            target="_blank">
-                            <svg class="bi" width="16" height="16">
-                                <use xlink:href="#arrow-right-circle" />
-                            </svg>
-                            Organizador
-                        </a>
-                    </li>
-                    <li>
-                        <a class="icon-link mb-1" href="https://github.com/twbs/examples/tree/main/webpack"
-                            rel="noopener" target="_blank">
-                            <svg class="bi" width="16" height="16">
-                                <use xlink:href="#arrow-right-circle" />
-                            </svg>
-                            ¿Buscas más?
-                        </a>
-                    </li>
-                </ul>
+    
+    <div id="myCarousel" class="carousel slide h-auto" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true"
+                aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner h-75">
+            <div class="carousel-item active h-100">
+                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
+                </svg>
+                <div class="container">
+                    <div class="carousel-caption text-start">
+                        <h1>Software</h1>
+                        <p class="opacity-75">Some representative placeholder content for the first slide of the
+                            carousel.</p>
+                        <p><a class="btn btn-lg btn-warning mostrar-btn">Ver más</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item h-100">
+                <!-- Reemplaza la imagen de fondo aquí -->
+                <img src="vista/assets/servicios.png" class="d-block w-100" alt="...">
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1>Servicios</h1>
+                        <p>Hacemos realidad tus ideas digitales.</p>
+                        <p><a class="btn btn-lg btn-warning mostrar-btn">Ver más</a></p>
+                    </div>
+                </div>
             </div>
 
-            <div class="col-md-6">
-                <h2 class="text-body-emphasis">Guias</h2>
-                <p>Read more detailed instructions and documentation on using or contributing to Bootstrap.</p>
-                <ul class="list-unstyled ps-0">
-                    <li>
-                        <a class="icon-link mb-1" href="/docs/5.3/getting-started/introduction/">
-                            <svg class="bi" width="16" height="16">
-                                <use xlink:href="#arrow-right-circle" />
-                            </svg>
-                            TiempoReal guía básica
-                        </a>
-                    </li>
-                    <li>
-                        <a class="icon-link mb-1" href="vista/assets/manualBasic.pdf" target="_blank">
-                            <svg class="bi" width="16" height="16">
-                                <use xlink:href="#arrow-right-circle" />
-                            </svg>
-                            Control de almacén guía básica
-                        </a>
-                    </li>
-                    <li>
-                        <a class="icon-link mb-1" href="/docs/5.3/getting-started/parcel/">
-                            <svg class="bi" width="16" height="16">
-                                <use xlink:href="#arrow-right-circle" />
-                            </svg>
-                            Organizador guía básica
-                        </a>
-                    </li>
-                </ul>
+            <div class="carousel-item h-100">
+                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
+                </svg>
+                <div class="container">
+                    <div class="carousel-caption text-end">
+                        <h1>Soluciones empresa</h1>
+                        <p>Some representative placeholder content for the third slide of this carousel.</p>
+                        <p><a class="btn btn-lg btn-warning mostrar-btn">Ver productos</a></p>
+                    </div>
+                </div>
             </div>
         </div>
-
+        <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+        <div class="carousel-item-expanded h-auto">
+            
+            <div class="texto-oculto-contenedor">
+            <hr class="w-75 mx-auto blanco">
+                <p class="m-3">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+            </div>
+            <div class="texto-oculto-contenedor">
+                <p>Este es el texto oculto de la sección 2.</p>
+            </div>
+            <div class="texto-oculto-contenedor">
+                <p>Este es el texto oculto de la sección 3.</p>
+            </div>
+        </div>
     </div>
-
 </div>
+
 <script>
     var div = document.querySelector('.containerCabecera');
-    var carousel = document.querySelector('#myCarousel');
+    var marketing = document.querySelector('.marketing');
     var altura = div.clientHeight;
-    carousel.style.marginTop = altura + 'px';
+    marketing.style.marginTop = altura + 'px';
+        var myCarousel = document.getElementById('myCarousel');
+        var carouselItems = myCarousel.querySelectorAll('.carousel-item');
+
+        // Detener el intervalo de desplazamiento automático cuando se hace clic en un botón del carousel
+        var carouselControls = myCarousel.querySelectorAll('.btn');
+        carouselControls.forEach(function (control) {
+            control.addEventListener('click', function () {
+                myCarousel.setAttribute('data-bs-ride', null);
+            });
+        });
+        var carouselNavButtons  = myCarousel.querySelectorAll('.carousel-control-prev, .carousel-control-next');
+        carouselNavButtons .forEach(function (button) {
+            button.addEventListener('click', function () {
+                let mostrar = document.querySelector('.mostrar-texto');
+                if(mostrar)
+                    mostrar.classList.remove('mostrar-texto');
+                myCarousel.setAttribute('data-bs-ride', 'carousel');
+            });
+        });
+        var botones = document.querySelectorAll('.mostrar-btn');
+        var textosOcultos = document.querySelectorAll('.texto-oculto-contenedor');
+
+        botones.forEach(function (boton, index) {
+            boton.addEventListener('click', function () {
+                // Agrega una clase para expandir el carousel
+                document.getElementById('myCarousel').classList.add('carousel-item-expanded');
+                // Muestra el texto oculto correspondiente
+                textosOcultos[index].classList.add('mostrar-texto');
+            });
+        });
 </script>
 
 <?php include 'vista/layout/footer.php'; ?>
