5651d0a2a705e5200b103e647ff23ea1e5f6d48e
Pablo Pinto <pablopintob.1@gmail.com>
2024-04-08 08:25:27 +0200

    sep

--- realidadvirtual.php
+++ realidadvirtual.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&display=swap">
 <link rel="stylesheet" href="vista/css/realidadvirtual.css">
 
-<div id="imgContent">
+<div id="imgContent" class="imagen">
         <div id="blanco">
             <img src="vista/assets/abuelos_portada.png" alt="">
         </div>
@@ -10,14 +10,14 @@
         <p id="der">EN REALIDAD</p><p id="der2">VIRTUAL</p>
 </div>
 <div id="myCarousel" class="carousel slide pt-3 mt-4 mb-6" data-bs-ride="carousel">
-    <div id="cuadro" class="w-75 p-4 rounded shadow">
+    <div id="cuadroDiv" class="cuadro w-75 p-4 rounded shadow">
         <p class="lead">¿Quieres conocernos mejor?</p>
         <a href="#" class="btn btn-primary btn-lg mt-5">
             Visita nuestra web
         </a>
     </div>
 </div>
-<div class="container-fluid pt-5    ">
+<div class="container-fluid pt-5">
     <h2 class="text-center">VIDAREAL</h2>
     <div id="columnas" class="row row-cols-sg-1 row-cols-lg-2 pt-2 mx-auto">
         <p class="p1">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
--- vista/js/realidadvirtual.js
+++ vista/js/realidadvirtual.js
@@ -1,36 +1,22 @@
-
     let menu = document.querySelector('.containerCabecera');
-    document.addEventListener('scroll', function() {
-        var div = document.querySelector('#imgContent');
-        var mitadAltura = div.offsetHeight / 2;
     
-        if (window.scrollY >= mitadAltura) {
-            menu.classList.remove('oculto');
-        } else {
-            menu.classList.add('oculto');
-        }
-    });
     function aplicarEstilosSegunTamanoPantalla() {
-        var blurryBackground = document.querySelector('.blurry-background');
-        var fondo = document.querySelector('#fondoDiv');
-        var marcasHeading = document.querySelector('#marcas h1');
+        var imagen = document.querySelector('#imgContent');
+        var cuadro = document.querySelector('#cuadroDiv');
         var altura = screen.height;
         var anchura = screen.width;
         var ratio = anchura/altura;
         var isHorizontal = window.matchMedia("(orientation: landscape)").matches;
         try {
             if (ratio > 1 || isHorizontal) {
-                blurryBackground.classList.remove('small-screen');
-                blurryBackground.classList.add('large-screen');
-                // Ajustar el tamaño del encabezado según la pantalla
-                marcasHeading.style.fontSize = "6vw";
+
             } else {
-                blurryBackground.classList.remove('large-screen');
-                blurryBackground.classList.add('small-screen');
-                fondo.classList.remove('fondo');
-                fondo.classList.add('min-fondo');
-                // Ajustar el tamaño del encabezado según la pantalla
-                marcasHeading.style.fontSize = "10vw";
+                imagen.classList.remove('imagen');
+                imagen.classList.add('min-imagen');
+                cuadro.classList.remove('cuadro');
+                cuadro.classList.add('min-cuadro');
+                menu.classList.remove('oculto');
+                //marcasHeading.style.fontSize = "10vw";
             }
         } catch (error) {
             alert("Error al aplicar estilos:", error);
@@ -39,4 +25,3 @@
     
     window.addEventListener('load', aplicarEstilosSegunTamanoPantalla);
     window.addEventListener('resize', aplicarEstilosSegunTamanoPantalla);
-    
