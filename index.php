892bdc32f1efcd013dab73cf742ae3372b8f7853
PaboPintoBiosca <pablopintob.1@gmail.com>
2024-04-15 16:16:46 +0200

    estoy en mi pick

--- index.php
+++ index.php
@@ -85,16 +85,16 @@
 </div>
 
 <!-- CONTACTO INFORMATE TERMINOS -->
-<div id="extras" class="container-fluid mt-5 pt-lg-5 row row-cols-sg-1 row-cols-mg-1 row-cols-lg-3 g-3 h-25 centrado">
+<div id="extras" class="container-fluid mt-5 pt-lg-5 row row-cols-1 row-cols-lg-3 g-2 centrado">
   <div class="row">
   <div class="col">
     <div class="card shadow-sm contenido">
       <div class="d-flex align-items-center smart">
-        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-chat-right-dots" viewBox="0 0 16 16" >
+        <svg xmlns="http://www.w3.org/2000/svg" width="12" fill="currentColor" class="bi bi-chat-right-dots h-75" viewBox="0 0 16 16" >
           <path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/>
           <path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
         </svg>
-        <p class="mb-0 ps-3">CONTACTO</p>
+        <p class="mb-0">CONTACTO</p>
       </div>
     </div>
   </div>
@@ -103,7 +103,7 @@
   <div class="col ">
     <div class="card shadow-sm contenido">
       <div class="d-flex align-items-center smart">
-        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-info-circle h-75" viewBox="0 0 16 16">
           <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
           <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
         </svg>
@@ -116,7 +116,7 @@
   <div class="col ">
     <div class="card shadow-sm contenido">
       <div class="d-flex align-items-center smart">
-        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-file-earmark-medical" viewBox="0 0 16 16">
+        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-file-earmark-medical h-75" viewBox="0 0 16 16">
           <path d="M7.5 5.5a.5.5 0 0 0-1 0v.634l-.549-.317a.5.5 0 1 0-.5.866L6 7l-.549.317a.5.5 0 1 0 .5.866l.549-.317V8.5a.5.5 0 1 0 1 0v-.634l.549.317a.5.5 0 1 0 .5-.866L8 7l.549-.317a.5.5 0 1 0-.5-.866l-.549.317zm-2 4.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1z"/>
           <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
         </svg>
