2e8fcaed6cab38c3a5e464d54715b98bfd6f8a06
PaboPintoBiosca <pablopintob.1@gmail.com>
2024-05-03 09:40:17 +0200

    pulleame esta

--- vista/js/realidadvirtual.js
+++ vista/js/realidadvirtual.js
@@ -3,24 +3,19 @@ var cuadro = document.querySelector('#imgContent');
 
 var menuLinks = document.querySelectorAll('nav ul li a');
   
-    const inputs = document.querySelectorAll('input[name="slide"]');
-    const descriptions = document.querySelectorAll('.texto .description');
     function aplicarEstilosSegunTamanoPantalla() {
         var ratio = screen.width/screen.height;
         var isHorizontal = window.matchMedia("(orientation: landscape)").matches;
+        cuadro.style.marginTop = menu.clientHeight + 'px';
         try {
             if (ratio > 1 || isHorizontal) {
-                menu.classList.add('oculto');
-                cuadro.style.marginTop = '0px';
             } else {
-                menu.classList.remove('oculto');
-                cuadro.style.marginTop = menu.clientHeight + 'px';
                 menuLinks.forEach(function(link) {
                     link.addEventListener('click', function() {
                       // Desmarcar el checkbox para ocultar el menú
                       document.getElementById("check").checked = false;
                     });
-                  });
+                });
             }
         } catch (error) {
             alert("Error al aplicar estilos:", error);
