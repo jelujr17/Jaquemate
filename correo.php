6dbd994b2bff5eb3ec1f23d88a83e3ffd73048b1
PaboPintoBiosca <pablopintob.1@gmail.com>
2024-05-08 11:50:25 +0200

    JUAS JUAS JUAS

--- correo.php
+++ correo.php
@@ -1,50 +1,3 @@
-<?php /*
-    if ($_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest' && $_SERVER['REQUEST_METHOD'] === 'POST') {
-        // Obtener el JSON del cuerpo de la solicitud
-        $json = file_get_contents('php://input');
-    
-        // Decodificar el JSON y manejar errores
-        $data = json_decode($json);
-        if ($data === null) {
-            echo 'Error: JSON inválido';
-            http_response_code(400); // Bad Request
-            exit;
-        }
-    
-        // Preparar datos para enviar por correo
-        $datosJson = array(
-            'nombre' => $data->nombre,
-            'email' => $data->email,
-            'asunto' => $data->asunto,
-            'mensaje' => $data->mensaje
-        );
-        $jsonData = json_encode($datosJson);
-    
-        // Enviar datos a la API externa
-        $ch = curl_init('http://jmvidareal.synology.me:8081/sendmail');
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json',
-            'Content-Length: ' . strlen($jsonData)
-        ));
-        $response = curl_exec($ch);
-    
-        // Verificar errores de cURL
-        if (curl_errno($ch)) {
-            echo 'Error cURL: ' . curl_error($ch);
-            http_response_code(500); // Internal Server Error
-        } else {
-            // Manejar la respuesta de la API
-            echo $response;
-        }
-    
-        // Cerrar la sesión cURL
-        curl_close($ch);
-    }*/
-?>
-
 <?php
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
@@ -64,12 +17,12 @@ if ($_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest' && $_SERVER['REQUEST_
 
     // Decodificar el JSON y manejar errores
     $data = json_decode($json);
-
-        $nombre = $data->nombre;
-        $email = $data->email;
-        $asunto = $data->asunto;
-        $mensaje = $data->mensaje;
-$mail = new PHPMailer(true);
+    $nombre = $data->nombre;
+    $email = $data->email;
+    $asunto = $data->asunto;
+    $mensaje = $data->mensaje;
+    $mail->CharSet = "UTF-8";
+    $mail = new PHPMailer(true);
 
 try {
     //Server settings
@@ -89,7 +42,7 @@ try {
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = $asunto;
-    $mail->Body    = `<html>
+    $mail->Body = "<html>
         <head>
             <style>
                 body {
@@ -116,42 +69,41 @@ try {
                     padding: 20px;
                 }
                 .footer {
-                    font-size : 14px;
+                    font-size: 14px;
                     background-color: #f0f0f0;
                     padding: 20px;
                     border-bottom-left-radius: 8px;
                     border-bottom-right-radius: 8px;
                 }
-                .mensaje{	
-                    text-align : center;
-                    font-size : 20px;
+                .mensaje {
+                    text-align: center;
+                    font-size: 20px;
                 }
-                .texto{	
+                .texto {
                     margin-left: 15px;
-                    text-align : left;
-                    font-size : 18px;
+                    text-align: left;
+                    font-size: 18px;
                 }
             </style>
         </head>
         <body>
-            <div class="container">
-                <div class="header">
-                    <h2><b>-- Asunto:  </b>$asunto</h2>
+            <div class='container'>
+                <div class='header'>
+                    <h2><b>-- Asunto:</b> $asunto</h2>
                 </div>
-                <div class="content">
-                    <p style="font-size: 18px;"><b>De:</b> $email</p>
-                    <p style="font-size: 18px;"><b>Nombre:</b> $nombre</p>
-                    <hr style="border-top: 1px solid #f0f0f0;">
-                    <p class="mensaje"><b>Mensaje</b></p>
-                    <p class="texto">$mensaje</p>
+                <div class='content'>
+                    <p style='font-size: 18px;'><b>De:</b> $email</p>
+                    <p style='font-size: 18px;'><b>Nombre:</b> $nombre</p>
+                    <hr style='border-top: 1px solid #f0f0f0;'>
+                    <p class='mensaje'><b>Mensaje</b></p>
+                    <p class='texto'>$mensaje</p>
                 </div>
-                <div class="footer">
+                <div class='footer'>
                     <p>Inazuma campeón</p>
                 </div>
             </div>
         </body>
-    </html>`;
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    </html>";
 
     $mail->send();
     echo 'Mensaje enviado';
